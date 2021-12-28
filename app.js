1dd9af48eadb3df8624c360b63412ed754db4317
thauburger <thauburger@gmail.com>
2021-12-28 01:40:29 -0800

    Add tjk subdomain

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 var staticdir = __dirname + '/public';
 console.log('Directory: ' + staticdir);
 
-app.use(express.favicon());
-app.use(express.static(staticdir));
-app.use(app.router);
-
 app.get('/', function(req, res, next) {
-  res.sendfile('public/index.html');
+  console.log(req.subdomains);
+  var filename = 'index.html';
+  if (req.subdomains.length == 1 && req.subdomains[0].toLowerCase() == 'tjk') {
+    filename = 'tjk.html'
+  }
+  res.sendFile(filename, {root: path.join(__dirname, 'public')});
 });
+
+app.use(express.static(staticdir));
 
 var port = process.env.PORT || 8080;
 app.listen(port);
